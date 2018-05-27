643bad1cf985b6051da43105cf23209778c401ce
Vikas Kumar <negi.vikash925@gmail.com>
2018-05-27 10:19:23 +0530

    Weather App using seperate custom weather service

--- controllers.js
+++ controllers.js
@@ -10,15 +10,14 @@ weatherApp.controller('homeController', ['$scope','$location', 'cityService', fu
 
 }]);
 
-weatherApp.controller('forecastController', ['$scope','$resource','$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
-     $scope.city = cityService.city;
-     $scope.weatherApi =$resource("http://api.openweathermap.org/data/2.5/forecast");
-     $scope.days = $routeParams.days || '2';
-     $scope.weatherResult = $scope.weatherApi.get({ q: $scope.city,cnt:$scope.days,appid:"4acf20ddbdf8e1d869a99f1ca2f30dcd"});
-     $scope.convertToFahrenheit = function(degK){
-     	return Math.round(1.8*(degK-273) + 32);
-     };
-     $scope.convertToDate = function(dt){
-     	return new Date(dt*1000);
-     };
+weatherApp.controller('forecastController', ['$scope','$routeParams', 'cityService', 'weatherService',function($scope, $routeParams, cityService, weatherService) {
+    $scope.city = cityService.city;
+    $scope.days = $routeParams.days || '2';
+    $scope.weatherResult = weatherService.GetWeather($scope.city, $scope.days);
+    $scope.convertToFahrenheit = function(degK){
+        return Math.round(1.8*(degK-273) + 32);
+    };
+    $scope.convertToDate = function(dt){
+    	return new Date(dt*1000);
+    };
 }]);
--- services.js
+++ services.js
@@ -2,3 +2,10 @@
 weatherApp.service('cityService', function () {
 	this.city = "New York";
 });
+weatherApp.service('weatherService', ['$resource',function($resource) {
+	this.GetWeather = function(city, days){
+		var weatherApi =$resource("http://api.openweathermap.org/data/2.5/forecast");
+		return weatherApi.get({ q: city,cnt:days,appid:"4acf20ddbdf8e1d869a99f1ca2f30dcd"});		
+	};
+	
+}]);
